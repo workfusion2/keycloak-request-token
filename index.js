975770f77f67bb87d923d76da761f2d1aa0fa9c2
Lucas Holmquist <lholmqui@redhat.com>
2016-05-11 10:30:06 -0400

    README and docs updates

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * @module keycloak-request-token
+ */
+
 'use strict';
 
 const http = require('http');
@@ -7,6 +11,35 @@ const querystring = require('querystring');
 
 const tokenUrl = 'protocol/openid-connect/token';
 
+/**
+  Requests a new Keycloak Access Token
+  @param {string} baseUrl - The baseurl for the Keycloak server - ex: http://localhost:8080/auth,
+  @param {object} settings - an object containing the settings
+  @param {string} settings.username - The username to login to the keycloak server - ex: admin
+  @param {string} settings.password - The password to login to the keycloak server - ex: *****
+  @param {string} settings.grant_type - the type of authentication mechanism - ex: password,
+  @param {string} settings.client_id - the id of the client that is registered with Keycloak to connect to - ex: admin-cli
+  @param {string} settings.realmName - the name of the realm to login to - defaults to 'masterg'
+  @returns {Promise} A promise that will resolve with the Access Token String.
+  @instance
+  @example
+
+  const tokenRequester = require('keycloak-request-token');
+  const baseUrl = 'http://127.0.0.1:8080/auth';
+  const settings = {
+      username: 'admin',
+      password: 'admi',
+      grant_type: 'password',
+      client_id: 'admin-cli'
+  };
+
+  tokenRequester(baseUrl, settings)
+    .then((token) => {
+      console.log(token);
+    }).catch((err) => {
+      console.log('err', err);
+    });
+ */
 function getToken(baseUrl, settings) {
   return new Promise((resolve, reject) => {
     settings = settings || {};
